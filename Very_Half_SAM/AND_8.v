4bb074c5398b260931333aba5c3cd46c5ee4907b
AF01Kurama <affaanfakih@gmail.com>
2025-03-23 15:03:02 +0530

    System Verilog with Convolution

--- Very_Half_SAM/AND_8.v
+++
@@ -1,9 +0,0 @@
-module AND_8(a,b,y);
-	input [7:0] a,b;
-	output [7:0] y;
-	
-	assign y = a & b;
-endmodule
-
-	
-	
--- WASHU_2_CPU/Custom_Signal_Package.vhd
+++
@@ -1,7 +0,0 @@
-library ieee;
-use ieee.std_logic_1164.all;
-
-package my_package is
-    type address is array (15 downto 0) of std_logic;
-	 subtype word is address;
-end package my_package;
